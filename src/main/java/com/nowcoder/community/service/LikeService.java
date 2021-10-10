3b4cd255202fee34a67da2f12bab4fad8f9f724f
stream1080 <fmxhax638@foxmail.com>
2021-10-10 09:17:56 +0800

    点赞

--- src/main/java/com/nowcoder/community/service/LikeService.java
+++ src/main/java/com/nowcoder/community/service/LikeService.java
@@ -17,7 +17,14 @@ public class LikeService {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    // 点赞
+    /**
+     * 点赞
+     *
+     * @param userId        点赞的用户
+     * @param entityType    被点赞的对象的类型：帖子，回复
+     * @param entityId      被点赞的对象的Id：
+     * @param entityUserId  被点赞的对象的作者的userId
+     */
     public void like(int userId, int entityType, int entityId, int entityUserId) {
         redisTemplate.execute(new SessionCallback() {
             @Override
@@ -25,6 +32,7 @@ public class LikeService {
                 String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
                 String userLikeKey = RedisKeyUtil.getUserLikeKey(entityUserId);
 
+                //查询该用户是否点赞->set中是否存在userId
                 boolean isMember = operations.opsForSet().isMember(entityLikeKey, userId);
 
                 operations.multi();
